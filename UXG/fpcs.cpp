3cfe0282aa34c7a23267a64212f9534ceb65f2e0
ABMS1 <jonk4m@gmail.com>
2020-06-25 16:53:55 -0400

    Added all project files

---
+++ UXG/fpcs.cpp
@@ -0,0 +1,6 @@
+#include "fpcs.h"
+
+Fpcs::Fpcs()
+{
+
+}
---
+++ UXG/fpcs.h
@@ -0,0 +1,14 @@
+#ifndef FPCS_H
+#define FPCS_H
+
+#include <QMainWindow>
+#include <QObject>
+#include <QWidget>
+
+class Fpcs
+{
+public:
+    Fpcs();
+};
+
+#endif // FPCS_H
